1d8714a822f46f4bd9223d4285b737007e8c5f7c
Edward Hsu <st89382000@gmail.com>
2021-02-22 22:02:14 +0800

    加入專案檔案。

---
+++ EdwardHsu.Lab.ActionBlockTest/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace EdwardHsu.Lab.ActionBlockTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ActionBlock<int> actionBlock = null;
+            actionBlock = new ActionBlock<int>(async time =>
+            {
+                await Task.Delay(time);
+                Console.WriteLine($"OK, 剩餘工作量:{actionBlock.InputCount}");
+            }, new ExecutionDataflowBlockOptions()
+            {
+                MaxDegreeOfParallelism = 2
+            });
+
+            actionBlock.Post(1000);
+            actionBlock.Post(1000);
+            actionBlock.Post(1000);
+            actionBlock.Post(1000);
+
+            actionBlock.Complete();
+
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            actionBlock.Completion.Wait();
+            sw.Stop();
+
+            Console.WriteLine("TOTLE TIME=" + sw.Elapsed.TotalSeconds);
+        }
+    }
+}
